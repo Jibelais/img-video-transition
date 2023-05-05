cd36c126666163d8b0070334488b914378dcd523
Jieun Pivarnik <JPivarnik@jis-macbook-pro.lan>
2023-05-04 23:32:07 -0500

    flickering solved

--- script.js
+++ script.js
@@ -7,35 +7,32 @@ const video = document.getElementById('main-video');
 image.style.display = 'block';
 video.style.display="none"
 
-function isInView(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-   
+const viewportHeight = window.innerHeight;
+
 function switchToVideo() {
-    image.style.opacity = 0;
-    setTimeout(function() {
-      image.style.display = "none";
-      video.style.display = "flex";
-      //video.style.transition = "width 3s ease-out"
-      video.style.clipPath= "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
-      video.currentTime = 0;
-      video.play();
-    }, 3000);
-  }
-  window.addEventListener("scroll", function() {
-    if (isInView(image)) {
-      this.switchToVideo();
-    } 
-    if (!isInView(video)){
+      image.style.opacity = 0;
+      setTimeout(function() {
+        image.style.display = "none";
+        video.style.display = "flex";
+        video.style.clipPath= "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
+        video.style.transition = "clip-path 3s ease-in-out"
+        video.play()
+      }, 3000);
+    }
+    
+window.addEventListener("scroll", () => {
+  const videoRect = video.getBoundingClientRect();
+
+  // if the top of the video is within the viewport
+  if (videoRect.top < viewportHeight) {
+    switchToVideo()
+  } else {
         image.style.opacity = 1;
         image.style.display = 'flex';
         video.style.display="none"
+        video.currentTime = 0;
         video.pause()
-    }
-  });
+  }
+});
+
+
